f3f8f4c8acdb94bcd9d68f4e01529b9a368913a1
HLIAS <HLIAS@HLIAS-PC>
2017-01-23 22:28:04 +0200

    Add project files.

---
+++ NewDinnerpartyCalc/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace NewDinnerpartyCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NumberOfGuests";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(117, 76);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Fancy";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(117, 123);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Healthy";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(117, 29);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(117, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cost";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NewDinnerpartyCalc/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewDinnerpartyCalc //Calculator for dinnerparty depended on guests, decor option and kind of drinks.
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+        public Form1()
+        {
+            InitializeComponent();
+
+            dinnerParty = new DinnerParty();
+            DisplayCost();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //change the total cost every time you check or uncheck the healthy box
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            DisplayCost();
+        }
+
+        //change the total cost every time you check or uncheck the fancy box
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            DisplayCost();
+        }
+
+        // change the total cost every time you change the number of quests
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) 
+        {
+            DisplayCost();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayCost() // check all properties and display the totalcost
+        {
+            dinnerParty.GuestsOfPartyCost((int)numericUpDown1.Value);
+            dinnerParty.CostOfDecors(checkBox1.Checked, (int)numericUpDown1.Value);
+            dinnerParty.CostOfDrinks(checkBox2.Checked, (int)numericUpDown1.Value);
+            textBox1.Text = dinnerParty.TotalCostOfParty(checkBox2.Checked, (int)numericUpDown1.Value).ToString("c");
+        }
+    }
+
+    public class DinnerParty
+    {
+        private int numberOfPeople;
+        private decimal DrinksCost;
+        private decimal FancyDecorsCosts;
+        private decimal TotalCost;
+        private int  GuestsCost;
+
+        
+        public int GuestsOfPartyCost(int People) //calculate the cost for the quests
+        {
+            numberOfPeople = People;
+            GuestsCost = numberOfPeople * 25;
+
+            return GuestsCost;
+
+        }
+        public decimal CostOfDecors(bool Fancy,int People) //calculate the cost of decors
+        {
+            numberOfPeople = People;
+            if (Fancy)
+            {
+                FancyDecorsCosts = (numberOfPeople * 15) + 50;
+                return FancyDecorsCosts;
+            }
+            else
+            {
+                FancyDecorsCosts = (numberOfPeople * 7.5m) + 30;
+                return FancyDecorsCosts;
+            }
+        }
+
+        public decimal CostOfDrinks (bool Healthy , int People)//calculate the cost of the drinks 
+        {
+            numberOfPeople = People;
+            if (Healthy)
+            {
+                DrinksCost = numberOfPeople * 5;
+                return DrinksCost;
+            }
+            else
+            {
+                DrinksCost = numberOfPeople * 20;
+                return DrinksCost;
+            }
+        }
+
+        public decimal TotalCostOfParty(bool Healthy, int People) // calculate the cost for all  the party
+        {
+            numberOfPeople = People;
+            if (Healthy)
+            {
+                TotalCost = (GuestsCost+ FancyDecorsCosts + DrinksCost) * 0.95m;
+                return TotalCost;
+            }
+            else
+            {
+                TotalCost = (GuestsCost + FancyDecorsCosts + DrinksCost);
+                return TotalCost;
+            }
+
+        }
+
+    }
+}
